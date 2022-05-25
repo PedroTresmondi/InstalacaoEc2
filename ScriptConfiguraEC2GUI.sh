787d8635a3027d950b33587dcb4c84540a81aaf0
PedroTresmondi <pedro.tresmondi@bandtec.com.br>
2022-05-24 21:00:49 -0300

    Atualizando tudo

--- ScriptConfiguraEC2GUI.sh
+++ ScriptConfiguraEC2GUI.sh
@@ -41,22 +41,41 @@ javac --version
 echo "Atualizando pacotes"
 sudo apt update && sudo apt upgrade -y
 
+echo "procurando o docker"
+which docker
+if [ $? = 0 ]
+then echo "Já possui docker instalado"
+else echo "Instalando docker"
+sudo apt install docker.io
+fi
+echo "iniciando o docker no sistema"
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "baixando mysql"
+sudo docker pull mysql:8.0.16
+echo "criando o banco de dados"
+sudo docker build -t omniview_img:1.0 .
+echo "executando mysql"
+sudo docker run -d -p 3306:3306 --name omniview omniview_img:1.0
+
+
 echo "Instalando protocolo RDP"
 sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y;
 
-cd Desktop
-
-git clone https://github.com/PedroTresmondi/Omniview.JAR.git
-
-cd Omniview.JAR/
-cd omniviewGUI/
-cd target/
-java -jar omniview-1.0-SNAPSHOT-jar-with-dependencies.jar
-
+clear
 
 echo " ███████ ██ ███    ███     ██████   █████      ██ ███    ██ ███████ ████████  █████  ██       █████   ██████  █████   ██████   "
 echo " ██      ██ ████  ████     ██   ██ ██   ██     ██ ████   ██ ██         ██    ██   ██ ██      ██   ██ ██      ██   ██ ██    ██  "
 echo " █████   ██ ██ ████ ██     ██   ██ ███████     ██ ██ ██  ██ ███████    ██    ███████ ██      ███████ ██      ███████ ██    ██  "
 echo " ██      ██ ██  ██  ██     ██   ██ ██   ██     ██ ██  ██ ██      ██    ██    ██   ██ ██      ██   ██ ██      ██   ██ ██    ██  "
 echo " ██      ██ ██      ██     ██████  ██   ██     ██ ██   ████ ███████    ██    ██   ██ ███████ ██   ██  ██████ ██   ██  ██████   "
-                                                                           
+
+
+echo "  ██████  ██████  ███    ██ ███████  ██████ ████████ ███████     ██████  ██████  ██████   "
+echo " ██      ██    ██ ████   ██ ██      ██         ██    ██          ██   ██ ██   ██ ██   ██  "
+echo " ██      ██    ██ ██ ██  ██ █████   ██         ██    █████       ██████  ██   ██ ██████   "
+echo " ██      ██    ██ ██  ██ ██ ██      ██         ██    ██          ██   ██ ██   ██ ██       "
+echo "  ██████  ██████  ██   ████ ███████  ██████    ██    ███████     ██   ██ ██████  ██       "
+                                                                                        
+                                                                                        
